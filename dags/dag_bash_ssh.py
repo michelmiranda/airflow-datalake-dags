78a8a5149461375936b5fe5cc2183b82e367242f
Michel Miranda <micheldsmiranda@gmail.com>
2021-04-08 22:26:34 -0300

    Update dag_bash_ssh.py

--- dags/dag_bash_ssh.py
+++ dags/dag_bash_ssh.py
@@ -18,7 +18,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('airflow_test', default_args=default_args)
+dag = DAG('airflow', default_args=default_args)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
